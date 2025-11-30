56a635a6db3e08e987c91d0f3e128d557b1eb51f
Far-Kamil <kamil200523@gmail.com>
2025-11-30 17:52:46 +0300

    second task completed

--- main.kt
+++ main.kt
@@ -1,30 +1,41 @@
-import kotlin.random.Random
+data class DriverApplication(
+    val carNumber: String,
+    val carBrand: String,
+    val carYear: Int
+)
+fun isValidNumber(s: String): Boolean {
+    if (s.length != 6) return false
 
-fun bubbleSort(arr: IntArray) {
-    val n = arr.size
-    for (i in 0 until n - 1) {
-        // Флаг для оптимизации: если за проход не было обменов — массив отсортирован
-        var swapped = false
-        for (j in 0 until n - i - 1) {
-            if (arr[j] > arr[j + 1]) {
-                // Обмен элементов
-                val temp = arr[j]
-                arr[j] = arr[j + 1]
-                arr[j + 1] = temp
-                swapped = true
-            }
-        }
-        // Если не было обменов — выходим
-        if (!swapped) break
+    // Разрешённые буквы: кириллица + латинские аналоги (в нижнем регистре для простоты)
+    val letters = "авекмнорстухabekmhopctyx" // ← все в lowercase!
+
+    val n = s.lowercase()  // приводим весь номер к нижнему регистру
+
+    return n[0] in letters &&
+           n[1].isDigit() &&
+           n[2].isDigit() &&
+           n[3].isDigit() &&
+           n[4] in letters &&
+           n[5] in letters
+}
+
+fun processApplications(apps: Array<DriverApplication>): Int {
+     val valid = apps.filter { app ->
+        app.carYear >= 2000 && isValidNumber(app.carNumber)
     }
+
+    // Группируем по марке и находим макс. количество
+    return valid.groupBy { it.carBrand }
+                .maxOfOrNull { it.value.size } ?: 0
 }
 fun main() {
-    val size = 10  // размер массива (можно изменить)
-    val maxValue = 100  // максимальное значение элемента (включительно)
-    val array = IntArray(size) { Random.nextInt(0, maxValue + 1) }
-    println("Origin array: ${array.contentToString()}")
-
-    bubbleSort(array)
-
-    println("Sorted array: ${array.contentToString()}")
+    val apps = arrayOf(
+        DriverApplication("а111аа", "Toyota", 2010),
+        DriverApplication("a111aa", "Toyota", 2015),
+        DriverApplication("X777XX", "Toyota", 2020),
+        DriverApplication("b222bb", "Lada", 2005),
+        DriverApplication("111aaa", "Lada", 2018), // не походит
+        DriverApplication("д555ке", "Lada", 2022)  // ('д' нет в списке)
+    )
+    println(processApplications(apps)) // → 3
 }
